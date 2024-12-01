6d871ca6050406cad7b42259321f2f435f8b1d3f
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-12-01 21:04:10 +0400

    add env

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     return (
         <>
             <h2 className={styles.h2}>react vite ts</h2>
+            <h2 className={styles.h2}>{import.meta.env.VITE_SOME_VARIABLE}</h2>
         </>
     )
 }
